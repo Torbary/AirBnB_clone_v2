3e8f1ab966badd5421d7e911ec9eabdd6e1d4d61
Akanni Emmanuel <akannie33@gmail.com>
2023-04-18 19:04:29 +0100

    delete object. added `delete` method to delete obj from the storage. updated `all` method to optionally filter the storage.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        else:
+            filter_objects = {}
+            for key, value in FileStorage.__objects.items():
+                if type(value) == cls:
+                    filter_objects[key] = value
+            return filter_objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +55,10 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        if obj is None:
+            pass
+        else:
+            key = obj.to_dict()['__class__'] + '.' + obj.id
+            del self.__objects[key]
