d99a0ef0d64778c44c20751150557a037c0340e1
Akanni Emmanuel <akannie33@gmail.com>
2023-04-20 17:28:36 +0100

    update City for Place.

--- models/city.py
+++ models/city.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 import os
-
+from sqlalchemy.orm import relationship
 hbnb_storage_type = os.getenv('HBNB_TYPE_STORAGE')
 
 
@@ -13,6 +13,8 @@ class City(BaseModel, Base):
         __tablename__ = 'cities'
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', backref='cities',
+                              cascade='all, delete-orphan')
     else:
         state_id = ""
         name = ""
