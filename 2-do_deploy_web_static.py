5e1d0806a4b4907c12c44c7608496efa51345210
Torbary <torbarey020@gmail.com>
2023-05-04 11:55:03 +0100

    Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to my web servers, using the function do_deploy

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Fabric script to deploy an archive to my web servers
+"""
+
+import os
+from fabric.api import env, put, run
+
+env.hosts = ["<IP web-01>", "<IP web-02>"]
+
+
+def do_deploy(archive_path):
+    """Distibutes an archive to the servers"""
+    if not os.path.exists(archive_path):
+        return False
+
+    filename = archive_path.split("/")[-1]
+    directory = "/data/web_static/releases/{}".format(filename.split(".")[0])
+
+    try:
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}/".format(directory))
+        run("tar -xzf /tmp/{} -C {}/".format(filename, directory))
+        run("rm /tmp/{}".format(filename))
+        run("mv {}/web_static/* {}/".format(directory, directory))
+        run("rm -rf {}/web_static".format(directory))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(directory))
+        print("New version deployed!")
+        return True
+    except ValueError:
+        return False
