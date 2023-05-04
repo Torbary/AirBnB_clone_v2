795cef7aeb5b12e262b8427703d00a117ce1bb2e
Torbary <torbarey020@gmail.com>
2023-05-04 11:21:17 +0100

    Bash script that sets up my web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,8 @@ if ! which nginx > /dev/null 2>&1; then
 fi
 
 # Create necessary directories if they don't exist
-sudo mkdir -p /data/web_static/{releases,test,shared}
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 
 # Create a fake HTML file to test Nginx configuration
 echo "<html>
@@ -23,7 +24,8 @@ echo "<html>
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
 # Set ownership of /data/ directory to ubuntu user and group recursively
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
 
 # Configure Nginx to serve content of /data/web_static/current to hbnb_static
 sudo sed -i '/listen 80 default_server;/a\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
