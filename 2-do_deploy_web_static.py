8d9f5d88d4d60954f502c93ef10e193768e562b9
Torbary <torbarey020@gmail.com>
2023-05-05 23:00:08 +0100

    istributes an archive to my web server

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,32 +1,51 @@
 #!/usr/bin/python3
-"""
-Fabric script to deploy an archive to my web servers
-"""
 
-import os
+import os.path
 from fabric.api import env, put, run
 
 env.hosts = ["34.207.211.110", "54.152.234.206"]
 
 
 def do_deploy(archive_path):
-    """Distibutes an archive to the servers"""
-    if not os.path.exists(archive_path):
+    """Distributes an archive to a web server.
+
+    Args:
+        archive_path (str): The path of the archive to distribute.
+
+    Returns:
+        bool: True if successful, False otherwise.
+    """
+    if not os.path.isfile(archive_path):
         return False
 
-    filename = archive_path.split("/")[-1]
-    directory = "/data/web_static/releases/{}".format(filename.split(".")[0])
+    archive_name = os.path.basename(archive_path)
+    name = os.path.splitext(archive_name)[0]
 
-    try:
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}/".format(directory))
-        run("tar -xzf /tmp/{} -C {}/".format(filename, directory))
-        run("rm /tmp/{}".format(filename))
-        run("mv {}/web_static/* {}/".format(directory, directory))
-        run("rm -rf {}/web_static".format(directory))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(directory))
-        print("New version deployed!")
-        return True
-    except ValueError:
+    if put(archive_path, f"/tmp/{archive_name}").failed:
         return False
+
+    if run(f"rm -rf /data/web_static/releases/{name}/").failed:
+        return False
+
+    if run(f"mkdir -p /data/web_static/releases/{name}/").failed:
+        return False
+
+    if run(f"tar -xzf /tmp/{archive_name} -C /data/web_static/releases/{name}/").failed:
+        return False
+
+    if run(f"rm /tmp/{archive_name}").failed:
+        return False
+
+    if run(f"mv /data/web_static/releases/{name}/web_static/* /data/web_static/releases/{name}/").failed:
+        return False
+
+    if run(f"rm -rf /data/web_static/releases/{name}/web_static").failed:
+        return False
+
+    if run("rm -rf /data/web_static/current").failed:
+        return False
+
+    if run(f"ln -s /data/web_static/releases/{name}/ /data/web_static/current").failed:
+        return False
+
+    return True
