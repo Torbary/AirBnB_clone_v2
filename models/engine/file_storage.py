1d77d6731c446d67e1a11ba4d4836725d7148718
Torbary <torbarey020@gmail.com>
2023-05-21 22:47:10 +0100

    file_Storage updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -63,3 +63,7 @@ class FileStorage:
         else:
             key = obj.to_dict()['__class__'] + '.' + obj.id
             del self.__objects[key]
+
+    def close(self):
+        """Deserializes the JSON file to object"""
+        self.reload()
