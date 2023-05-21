2b9e9703688fc0c9788defcd3d3d08527394edd0
Torbary <torbarey020@gmail.com>
2023-05-21 22:47:24 +0100

    updated

--- models/state.py
+++ models/state.py
@@ -17,6 +17,7 @@ class State(BaseModel, Base):
         name = Column(String(128), nullable=False)
         cities = relationship('City', backref='state',
                               cascade='all, delete-orphan')
+
     else:
         name = ""
 
