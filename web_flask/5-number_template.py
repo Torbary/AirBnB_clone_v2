546ae44cad13f2a851caa5b1c5a3abfd171e3b68
Torbary <torbarey020@gmail.com>
2023-05-21 09:40:51 +0100

    5-number_template.py

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""start a Flask application"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Route handler for the root endpoint."""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Route handler for the /hbnb endpoint."""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_text_c(text):
+    """Route handler for the /c/<text> endpoint."""
+    # Replace underscore with a space
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_text_python(text):
+    """Route handler for the /python/<text> endpoint."""
+    # Replace underscore with a space
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    """Route handler for the /number/<n> endpoint."""
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def display_number_template(n):
+    """Route handler for the /number_template/<n> endpoint."""
+    return render_template('5-number.html', number=n)
+
+
+if __name__ == '__main__':
+    # Run the Flask application on 0.0.0.0:5000
+    app.run(host='0.0.0.0', port=5000)
